6d3dce2aef83abbf32778894c0d45187ff36cc74
Richard Melchior <raustin.melchior@gmail.com>
2018-06-02 20:43:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,5 +7,6 @@ def line(katz_deli)
   if(katz_deli == [])
     puts "The line is currently empty."
   else
+    "The line is currently #{katz_deli.each_with_index { |name, index| "#{index + 1}. #{name}"}}";
   end
 end
