134cb64b7af41e32585bf4c7b22a9b59dbc2faed
Richard Melchior <raustin.melchior@gmail.com>
2018-06-02 20:36:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+katz_deli = [];
+
+##
+# => #line takes in the line array and displays everyone's place in line
+##
+def line()
+  if(katz_deli == [])
+    puts "The line is currently empty."
+  else
+  end
+end
