79416acbab1f16047b6454269dfb0b131d314b7e
Richard Melchior <raustin.melchior@gmail.com>
2018-06-02 21:32:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,10 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length + 1} in line.";
   katz_deli.push(name);
 end
+
+def now_serving(katz_deli)
+  if(katz_deli.length == 0)
+    puts "There is currently nobody in line."
+  else 
+    puts "Now serving #{katz_deli.shift()}."
+end
